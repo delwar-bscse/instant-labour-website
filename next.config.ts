6913774123c2ed8ee64993da66323a74ab2d20ce
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-12-27 17:41:54 +0600

    live server link

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: "http",
         hostname: "10.10.7.50",
         port: "5002",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "asad5002.binarybards.online",
         pathname: "/images/**",
       },
     ],
