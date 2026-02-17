893fadb01bef6aa3f5f60433acc50d90647c9ed6
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-02-17 09:38:01 +0600

    image issues.....

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,43 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "76.13.5.2",
+//         port: "5000",
+//         pathname: "/images/**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "**",
+//         pathname: "/images/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//         pathname: "/images/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from "next";
+ 
 const nextConfig: NextConfig = {
+  /* config options here */
+ 
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "**",
-        pathname: "/images/**",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-        pathname: "/images/**",
-      },
+    domains: [
+      "i.ibb.co.com",
+      "76.13.5.2",
     ],
   },
 };
-
+ 
 export default nextConfig;
+ 
+ 
